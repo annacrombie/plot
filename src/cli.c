c2a9940c6667aa34b0aa71c05bf35ddc94570458
Stone Tickle <lattis@mochiro.moe>
2019-05-06 09:31:51 -0500

    add stdin reading feature, array can be piped!

--- src/cli.c
+++ src/cli.c
@@ -1,7 +1,59 @@
+#include <ctype.h>
 #include "plot.h"
+
+#define ARR_GROW_INC 5
+
+size_t read_arr(double **arr) {
+  double *narr, *tarr;
+  char buf[20] = "";
+  char rd[1] = "";
+  size_t t, bon, arr_len, arr_cap;
+  t = bon = arr_len = arr_cap = 0;
+
+  for (;bon < 20; bon++) buf[bon] = '\0';
+  bon = 0;
+
+  *arr = calloc(ARR_GROW_INC, sizeof(double));
+  if (NULL == *arr) {
+    fprintf(stderr, "out of memory\n");
+    exit(1);
+  }
+  arr_cap += ARR_GROW_INC;
+
+  while (1) {
+    t = fread(rd, 1, sizeof(char), stdin);
+    if (t == 0) return arr_len;
+
+    if (!isspace(*rd)) {
+      buf[bon] = *rd;
+      bon++;
+    } else {
+      (*arr)[arr_len] = strtod(buf, NULL);
+      arr_len += 1;
+
+      while (bon>0) {
+        buf[bon] = '\0';
+        bon--;
+      }
+
+      if (arr_len >= arr_cap) {
+        narr = calloc(arr_cap + ARR_GROW_INC, sizeof(double));
+        memcpy(narr, *arr, sizeof(double) * arr_cap);
+        tarr = *arr;
+        *arr = narr;
+        free(tarr);
+        arr_cap += ARR_GROW_INC;
+      }
+    }
+  }
+}
 
 double *make_arr(int argc, char **argv) {
   double *arr = calloc(argc, sizeof(double));
+  if (NULL == arr) {
+    fprintf(stderr, "out of memory\n");
+    exit(1);
+  }
   for (int i=0;i<argc;i++) {
     arr[i] = strtod(argv[i], NULL);
   }
@@ -56,10 +108,16 @@ int main(int argc, char **argv) {
   }
 
   /* Get the array from the rest of the options */
-  int arrlen = argc - i;
+  size_t arrlen = argc - i;
   double *arr;
   if (arrlen < 1) { print_help(); return 1; }
-  arr = make_arr(arrlen, &argv[i]);
+
+  if (strcmp(argv[i], "-") == 0) {
+    arrlen = read_arr(&arr);
+    if (arrlen < 1) { return 0; }
+  } else {
+    arr = make_arr(arrlen, &argv[i]);
+  }
 
   plot(height, label_format, arrlen, arr);
   free(arr);
