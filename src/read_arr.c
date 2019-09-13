43eaa3219fb8fe82daa2b337eb842142c710ac55
Stone Tickle <lattis@mochiro.moe>
2019-09-13 10:28:31 +0900

    use is_digit in read_arr

--- src/read_arr.c
+++ src/read_arr.c
@@ -13,9 +13,7 @@ size_t read_arr(FILE *f, double **arr, size_t maxlen)
 	*arr = safe_calloc(maxlen, sizeof(double));
 
 	while (fread(buf, 1, sizeof(char), f) != 0) {
-		switch (*buf) {
-		case '0': case '1': case '2': case '3': case '4':
-		case '5': case '6': case '7': case '8': case '9':
+		if (is_digit(*buf)) {
 			if (numbuf_i == MAX_NUM_LEN - 1) {
 				fprintf(stderr, "number too long\n");
 				exit(EXIT_FAILURE);
@@ -24,18 +22,14 @@ size_t read_arr(FILE *f, double **arr, size_t maxlen)
 			numbuf[numbuf_i] = *buf;
 			numbuf[numbuf_i + 1] = '\0';
 			numbuf_i++;
-			break;
-		default:
+		} else {
 			(*arr)[len] = strtod(numbuf, NULL);
 			len++;
 			numbuf_i = 0;
 			if (len == maxlen)
 				return maxlen;
-			break;
-
 		}
 	}
 
 	return len;
 }
-
