db7f575dc7d143e870d93a0ef26b02051754d9f4
Stone Tickle <lattis@mochiro.moe>
2019-09-13 15:02:18 +0900

    allow floats to be input!

--- src/util.c
+++ src/util.c
@@ -29,7 +29,7 @@ int is_digit(char c)
 	switch (c) {
 	case '0': case '1': case '2': case '3': case '4':
 	case '5': case '6': case '7': case '8': case '9':
-	case '-':
+	case '-': case '.':
 		return 1;
 	default:
 		return 0;
