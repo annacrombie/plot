957f30aa238d3670570b330e7dde4b4aa459e781
Adrian DC <radian.dc@gmail.com>
2021-01-17 12:14:17 +0100

    test: minor codestyle improvements and ensure binary parameter

--- tests/follow.sh
+++ tests/follow.sh
@@ -1,19 +1,21 @@
-#!/bin/sh -eux
+#!/bin/sh
+set -eux
 
-plot="$1"
+# Parameters
+plot="${1?}"
 
 # Follow stream
-seq 1 100 | shuf | timeout -sSIGINT 3 "$plot" -c Y -f -S 200 -x 5: -y 10:3:1 || [ "${?}" -eq 124 ]
+seq 1 100 | shuf | timeout -sSIGINT 3 "${plot}" -c Y -f -S 200 -x 5: -y 10:3:1 || [ "${?}" -eq 124 ]
 
 # Pre-bounded stream
-seq 1 100 | timeout -sSIGINT 3 "$plot" -b 0:100 -c Y -f -S 200 -x 5: -y 10:3:1 || [ "${?}" -eq 124 ]
+seq 1 100 | timeout -sSIGINT 3 "${plot}" -b 0:100 -c Y -f -S 200 -x 5: -y 10:3:1 || [ "${?}" -eq 124 ]
 
 # Follow file
 seq 1 100 | shuf >tmp1
-timeout -sSIGINT 3 "$plot" -c Y -f -i tmp1 -S 200 -x 5: -y 10:3:1 || [ "${?}" -eq 124 ]
+timeout -sSIGINT 3 "${plot}" -c Y -f -i tmp1 -S 200 -x 5: -y 10:3:1 || [ "${?}" -eq 124 ]
 
 # Follow files
 seq 1 100 | shuf >tmp1
 seq 1 150 | shuf >tmp2
 seq 1 200 | shuf >tmp3
-timeout -sSIGINT 3 "$plot" -c C -f -i tmp1 -c R -i tmp2 -c G -i tmp3 -S 200 -x 5: -y 10:3:1 || [ "${?}" -eq 124 ]
+timeout -sSIGINT 3 "${plot}" -c C -f -i tmp1 -c R -i tmp2 -c G -i tmp3 -S 200 -x 5: -y 10:3:1 || [ "${?}" -eq 124 ]
--- tests/valgrind.sh
+++ tests/valgrind.sh
@@ -1,9 +1,12 @@
-#!/bin/sh -eux
+#!/bin/sh
+set -eux
 
-plot="$1"
+# Parameters
+plot="${1?}"
 
-vg_opts="--leak-check=yes --track-origins=yes --exit-on-first-error=yes --error-exitcode=1"
+# Configurations
+vg_opts='--leak-check=yes --track-origins=yes --exit-on-first-error=yes --error-exitcode=1'
 
 # Memory leaks
-seq 1 100 | shuf | valgrind $vg_opts "$plot" -c Y -x 5: -y 10:3:1
-seq 1 100 | shuf | timeout -sSIGINT 3 valgrind $vg_opts "$plot" -c Y -f -x 5: -y 10:3:1 || [ "${?}" -eq 124 ]
+seq 1 100 | shuf | valgrind ${vg_opts} "${plot}" -c Y -x 5: -y 10:3:1
+seq 1 100 | shuf | timeout -sSIGINT 3 valgrind ${vg_opts} "${plot}" -c Y -f -x 5: -y 10:3:1 || [ "${?}" -eq 124 ]
