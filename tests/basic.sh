f97ab55ad611014b2fc291e6cae81c722563abc6
Stone Tickle <lattis@mochiro.moe>
2021-01-17 09:32:54 +0900

    put temporary files in meson workdir

--- tests/basic.sh
+++ tests/basic.sh
@@ -30,14 +30,12 @@ seq 1 1000 | shuf | "$plot" -c M -x 5:::2 -y 10:3:2
 seq 1 1000 | shuf | "$plot" -c C -x 5:0:0:3:blue -y 10:3:3
 
 # Animate files
-seq 1 100 | shuf >./tests/tmp.1.raw
-seq 1 150 | shuf >./tests/tmp.2.raw
-seq 1 200 | shuf >./tests/tmp.3.raw
-"$plot" -c C -A -i ./tests/tmp.1.raw -c R -i ./tests/tmp.2.raw -c G -i ./tests/tmp.3.raw -S 200 -x 5: -y 10:3:1
-rm -f ./tests/tmp.1.raw ./tests/tmp.2.raw ./tests/tmp.3.raw
+seq 1 100 | shuf >tmp1
+seq 1 150 | shuf >tmp2
+seq 1 200 | shuf >tmp3
+"$plot" -c C -A -i tmp1 -c R -i tmp2 -c G -i tmp3 -S 200 -x 5: -y 10:3:1
 
 # Merge files
-seq 1 150 | shuf >./tests/tmp.1.raw
-seq 1 100 | shuf >./tests/tmp.2.raw
-"$plot" -a2 -c C -i ./tests/tmp.1.raw -c R -i ./tests/tmp.2.raw -m -S 200 -x 10: -y 10:3:1
-rm -f ./tests/tmp.1.raw ./tests/tmp.2.raw
+seq 1 150 | shuf >tmp1
+seq 1 100 | shuf >tmp2
+"$plot" -a2 -c C -i tmp1 -c R -i tmp2 -m -S 200 -x 10: -y 10:3:1
--- tests/follow.sh
+++ tests/follow.sh
@@ -9,13 +9,11 @@ seq 1 100 | shuf | timeout -sSIGINT 3 "$plot" -c Y -f -S 200 -x 5: -y 10:3:1 || 
 seq 1 100 | timeout -sSIGINT 3 "$plot" -b 0:100 -c Y -f -S 200 -x 5: -y 10:3:1 || [ "${?}" -eq 124 ]
 
 # Follow file
-seq 1 100 | shuf >./tests/tmp.raw
-timeout -sSIGINT 3 "$plot" -c Y -f -i ./tests/tmp.raw -S 200 -x 5: -y 10:3:1 || [ "${?}" -eq 124 ]
-rm -f ./tests/tmp.raw
+seq 1 100 | shuf >tmp1
+timeout -sSIGINT 3 "$plot" -c Y -f -i tmp1 -S 200 -x 5: -y 10:3:1 || [ "${?}" -eq 124 ]
 
 # Follow files
-seq 1 100 | shuf >./tests/tmp.1.raw
-seq 1 150 | shuf >./tests/tmp.2.raw
-seq 1 200 | shuf >./tests/tmp.3.raw
-timeout -sSIGINT 3 "$plot" -c C -f -i ./tests/tmp.1.raw -c R -i ./tests/tmp.2.raw -c G -i ./tests/tmp.3.raw -S 200 -x 5: -y 10:3:1 || [ "${?}" -eq 124 ]
-rm -f ./tests/tmp.1.raw ./tests/tmp.2.raw ./tests/tmp.3.raw
+seq 1 100 | shuf >tmp1
+seq 1 150 | shuf >tmp2
+seq 1 200 | shuf >tmp3
+timeout -sSIGINT 3 "$plot" -c C -f -i tmp1 -c R -i tmp2 -c G -i tmp3 -S 200 -x 5: -y 10:3:1 || [ "${?}" -eq 124 ]
