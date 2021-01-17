2a0789ab7693e13e35a94a17b8635e07ebe3061c
Stone Tickle <lattis@mochiro.moe>
2021-01-17 09:49:24 +0900

    tell meson about doc dependencies

--- docs/inserter.py
+++ docs/inserter.py
@@ -9,7 +9,8 @@ SEP = "@"
 with open(infile, "r") as f:
     for line in f:
         if line[0] == SEP and line[-2] == SEP:
-            path = line[1:-2]
+            num = int(line[1:-2]) + 2
+            path = sys.argv[num]
             with open(path, "r") as inf:
                 print(inf.read(), end="")
         else:
