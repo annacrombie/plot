39521d5de9704dbc38284b4b53eb5fb9146bdb95
Stone Tickle <lattis@mochiro.moe>
2021-01-17 10:51:46 +0900

    fix valgrind opts in tests

--- tests/valgrind.sh
+++ tests/valgrind.sh
@@ -2,15 +2,8 @@
 
 plot="$1"
 
-valgrind_()
-{
-	valgrind --leak-check=yes \
-		--track-origins=yes \
-		--exit-on-first-error=yes \
-		--error-exitcode=1 \
-		"$@"
-}
+vg_opts="--leak-check=yes --track-origins=yes --exit-on-first-error=yes --error-exitcode=1"
 
 # Memory leaks
-seq 1 100 | shuf | valgrind_ "$plot" -c Y -x 5: -y 10:3:1
-seq 1 100 | shuf | timeout -sSIGINT 3 valgrind_ "$plot" -c Y -f -x 5: -y 10:3:1 || [ "${?}" -eq 124 ]
+seq 1 100 | shuf | valgrind $vg_opts "$plot" -c Y -x 5: -y 10:3:1
+seq 1 100 | shuf | timeout -sSIGINT 3 valgrind $vg_opts "$plot" -c Y -f -x 5: -y 10:3:1 || [ "${?}" -eq 124 ]
