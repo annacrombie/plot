9cf925e267a4470a936f10957f9d621e14286b46
Stone Tickle <lattis@mochiro.moe>
2021-02-12 19:24:44 +0900

    refactor/clarify read_numbers around dbuf

--- src/input.h
+++ src/input.h
@@ -1,8 +1,21 @@
-#ifndef __READ_ARR_H
-#define __READ_ARR_H
+#ifndef INPUT_H
+#define INPUT_H
 
 #include <stdio.h>
+#include <stdbool.h>
 
-#include "plot.h"
+#include "data_proc.h"
 
+#define MAX_INBUF 1024
+struct input {
+	char buf[MAX_INBUF + 1]; // + 1 so we can always be sure there will be
+	                         // a sentinel 0 at the end, as long as
+	                         // everyone abides by MAX_INBUF
+	FILE *src;
+	size_t rem; // the number of remaining bytes left over in buf
+	struct dbuf out;
+};
+
+
+bool input_read(struct input *in);
 #endif
