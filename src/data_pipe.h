60a2eb92bdb9d075287d4a95eb6199c321fd2b51
Stone Tickle <lattis@mochiro.moe>
2021-02-15 15:41:44 +0900

    add pipeline flags with a rewind flag

--- src/data_pipe.h
+++ src/data_pipe.h
@@ -6,7 +6,11 @@
 #include "data_proc.h"
 #include "plot.h"
 
-bool pipeline_create(const char *path);
+enum pipeline_flags {
+	pipeline_flag_rewind = 1 << 0,
+};
+
+bool pipeline_create(const char *path, uint8_t pipeline_flags);
 bool pipeline_append(enum data_proc_type proc, void *ctx, uint32_t size);
 bool pipeline_exec_all(struct plot *p, uint32_t max_new);
 void pipeline_reset_eofs(void);
